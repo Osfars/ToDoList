96f69b2576e7b09f522e54afcac44292e99f0258
Farouk <farouk.sumana@outlook.com>
2024-07-31 03:45:55 +0000

    # add event listener to remove item from the list and also span itam(s)

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputbox.value;
         listcontainer.appendChild(li);
-        inputbox.value='';
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputbox.value='';
 }
+
+listcontainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+
+}, false);
